5b59c2ef85be2dd415754369ebbdf9c95b93e642
sofiane <khoudoursofiane75@gmail.com>
2018-11-26 19:26:31 +0100

    first step

--- app/src/main/java/com/hackathon/saveme/MainActivity.java
+++ app/src/main/java/com/hackathon/saveme/MainActivity.java
@@ -2,12 +2,44 @@ package com.hackathon.saveme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView text = (TextView)findViewById(R.id.textView);
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (i){
+
+                    case 0:{
+                        text.setText("Hello");
+                        i++;
+                        i = (i < 3) ? i: 0;
+                        break;
+                    }
+                    case 1:{
+                        text.setText("Champions");
+                        i++;
+                        i = (i < 3) ? i: 0;
+                        break;
+                    }
+                    case 2:{
+                        text.setText("Never look back");
+                        i++;
+                        i = (i < 3) ? i: 0;
+                        break;
+                    }
+                }
+            }
+        });
     }
 }
