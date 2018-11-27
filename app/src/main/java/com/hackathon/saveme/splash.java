6413de171adaf8d00c8220d8fef45eb44cf25b9d
sofiane <khoudoursofiane75@gmail.com>
2018-11-27 13:16:53 +0100

    splash screen last longer with a fullscreen

--- app/src/main/java/com/hackathon/saveme/splash.java
+++ app/src/main/java/com/hackathon/saveme/splash.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 public class splash extends AppCompatActivity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
