e4b707886e498059ee3a9a76f5759beb12573354
sofiane <khoudoursofiane75@gmail.com>
2018-12-07 01:40:56 +0100

    json working sending to server working

--- app/src/main/java/com/hackathon/saveme/emergencyActivity.java
+++ app/src/main/java/com/hackathon/saveme/emergencyActivity.java
@@ -4,12 +4,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,45 +28,126 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class emergencyActivity extends AppCompatActivity {
     android.hardware.Camera cam;
     android.hardware.Camera.Parameters p;
+    ArrayList<String> userDataCollected = new ArrayList<String>();
     int k = 0;
     private Vibrator v;
+
+    private void readUserData() {
+        File path = getFilesDir();
+        File file = new File(path, "userdata");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                userDataCollected.add(line);
+
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (EOFException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        readUserData();
         setContentView(R.layout.activity_emergency);
         v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
 
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                BufferedOutputStream os = null;
-                InputStream is = null;
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+                JSONObject jsonObject = new JSONObject();
                 try {
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("lat", "3333");
-                    jsonObject.put("lon", "8888");
-                    byte[] enco = jsonObject.toString().getBytes("UTF-8");
-                    String encoded = Base64.encodeToString(enco,Base64.DEFAULT);
-                    URLConnection url = new URL("http://192.168.1.61/emer?json="+encoded).openConnection();
-                    url.setRequestProperty("Accept-Charset","utf-8");
-                    is = new BufferedInputStream(url.getInputStream());
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    jsonObject.put("name", userDataCollected.get(0));
+                    jsonObject.put("email", userDataCollected.get(1));
+                    jsonObject.put("allergy", userDataCollected.get(2));
+                    jsonObject.put("image", new String(Base64.encode(userDataCollected.get(3).getBytes(), Base64.DEFAULT)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String dataF = jsonObject.toString();
+
+                byte[] encodedJson = Base64.encode(dataF.getBytes(), Base64.DEFAULT);
+                String url = null;
+                try {
+                    url = "http://192.168.1.61/emer?json=" + URLEncoder.encode(dataF, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Log.d("sending to ", url);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response : ", response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", error.getMessage());
+                    }
+                });
+
+             /*  OutputStreamWriter os = null;
+                InputStreamReader is = null;
+                try {
+                    String message1 = "3.4888888444";
+                    String message = "44.5555555;55";
+                 String name = "sofiane";
+                    Log.d("hzheh",name);
+                    String parameters = "lat="+message1+"&lon="+message;
+                    URL url = new URL("http://192.168.1.61/emer");
+                    HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                    connection.setRequestMethod("GET");
+
+os = new OutputStreamWriter(connection.getOutputStream());
+os.write(parameters);
+os.flush();
+os.close();
+connection.connect();
+if(connection.getResponseCode() >=200 && connection.getResponseCode() < 400) {
+    is = new InputStreamReader(connection.getInputStream());
+}else{
+    is = new InputStreamReader(connection.getErrorStream());
+}
+                    Log.d("response",Integer.toString(connection.getResponseCode()));
+//TODO fix send name
+
+
+                    BufferedReader reader = new BufferedReader(is);
                     StringBuilder result = new StringBuilder();
                     String line;
                     while ((line = reader.readLine()) != null) {
@@ -64,33 +156,22 @@ public class emergencyActivity extends AppCompatActivity {
                     }
 
                     Log.d("Results : ",result.toString());
-
+is.close();
+reader.close();
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
-
+                } catch (EOFException r){
+                    r.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } finally {
 
-                    try {
-                        if(os != null){
-                            os.close();
 
-                        }
-                        if(is != null){
-                            is.close();
-                        }
+                }*/
+                queue.add(stringRequest);
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
             }
-        }).start();
 
+        }).start();
 
       /*  new Thread(new Runnable() {
             @Override
@@ -128,9 +209,8 @@ public class emergencyActivity extends AppCompatActivity {
         }).start();
 */
         LocalBroadcastManager.getInstance(this).registerReceiver(positionReciver, new IntentFilter("position"));
-/*
 
-        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+        /*if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
             Timer t = new Timer();
             t.scheduleAtFixedRate(new TimerTask() {
                 @Override
@@ -159,8 +239,7 @@ public class emergencyActivity extends AppCompatActivity {
 
 
             Toast.makeText(getApplicationContext(),"It has flash",Toast.LENGTH_LONG).show();
-        }
-        */
+        }*/
     }
 
     private BroadcastReceiver positionReciver = new BroadcastReceiver() {
@@ -170,34 +249,36 @@ public class emergencyActivity extends AppCompatActivity {
             final String message1 = intent.getStringExtra("lat");
             Log.d("Lat", message);
             Log.d("lon", message1);
-
+/*
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    BufferedOutputStream os = null;
+                    InputStream is = null;
                     try {
-                        URL url = new URL("http://192.168.1.61:8080/SaveMeWeb/dataReciver/index.php");
-                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                        connection.setDoInput(true);
-                        connection.setDoInput(false);
-                        connection.setRequestMethod("POST");
-                        connection.connect();
-                        OutputStream outputStream = new BufferedOutputStream(connection.getOutputStream());
-                        JSONObject jsonObject = new JSONObject();
-                        jsonObject.put("lat", message1);
-                        jsonObject.put("lon", message);
-                        outputStream.write(jsonObject.toString().getBytes());
-                        outputStream.flush();
-                        outputStream.close();
+                        URLConnection url = new URL("http://192.168.43.215:8080/emer?lat="+message1+"&lon="+message).openConnection();
+                        Log.d("State :","Starting");
+
+
+                        url.setRequestProperty("Accept-Charset","utf-8");
+                        is = new BufferedInputStream(url.getInputStream());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                        StringBuilder result = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line);
+
+                        }
+
+                        Log.d("Results : ",result.toString());
 
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
                 }
-            }).start();
+            }).start();*/
         }
     };
 }
